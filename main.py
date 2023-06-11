6a82af217863eef7d4b8aee7778d98e0330caf30
jackpa <jackson.pacheco@dolarview.com.br>
2023-06-11 06:58:05 -0300

    feat(): Add method get number of lines.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input("Enter amount to deposit: ")
@@ -13,3 +15,24 @@ def deposit():
     return amount
 
 
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 0 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Amount must be a number")
+
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print("Balance: " + str(balance))
+    print("Lines: " + str(lines))
+
+main()
